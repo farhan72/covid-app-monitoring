6282e3d0bbd7b585cce78eb4870b94c28018cbfd
alfiy <alfi.cakap@gmail.com>
2020-07-03 00:28:36 +0700

    layouting

---
+++ src/components/Layout.vue
@@ -0,0 +1,31 @@
+<template>
+  <Header />
+</template>
+
+<script>
+import $ from "jquery";
+import Header from "../components/pages/Header";
+export default {
+  name: "Layout",
+  components: {
+    Header,
+  },
+  methods: {
+    hamburgerMenu() {
+      $(document).ready(function() {
+        // Check for click events on the navbar burger icon
+        $(".navbar-burger").click(function() {
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          $(".navbar-burger").toggleClass("is-active");
+          $(".navbar-menu").toggleClass("is-active");
+        });
+      });
+    },
+  },
+  created: function() {
+    this.hamburgerMenu();
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
